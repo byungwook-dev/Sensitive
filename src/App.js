3f6a878a5c4e26e315533f36e99f115dc1ad112b
byungwook-dev <byungwook.dev@gmail.com>
2026-04-04 17:28:43 +0900

    도커 버전 18로 회귀

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Hello Sensitive Project!</h1>
+      <p>This is the React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
